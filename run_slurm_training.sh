b115f44a979699b19958bb1bb90ac8e4a94be88a
Dima <zhylko.dima@gmail.com>
2024-04-14 23:55:04 -0400

    fix launch script

--- run_slurm_training.sh
+++ run_slurm_training.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # shellcheck disable=SC2206
 #SBATCH --job-name=distil-herbert
-SBATCH -p nvidia
+#SBATCH -p nvidia
 #SBATCH --cpus-per-task=64
 #SBATCH --mem=480GB
 #SBATCH --nodes=2
 #SBATCH --tasks-per-node=1
 #SBATCH --gpus-per-node=4
 
-#SBATCH --time=24:00:00
-
-set -x
+#SBATCH --time=96:00:00
 
 eval "$(conda shell.bash hook)"
 conda activate distil
+
+set -x
 
 nodes=$(scontrol show hostnames "$SLURM_JOB_NODELIST")
 nodes_array=($nodes)
@@ -25,7 +25,9 @@ port=29400
 ip_head=$head_node_ip:$port
 export ip_head
 
-srun --nodes=1 --ntasks=1 -w "$head_node" \
+export WANDB_PROJECT=distil-herbert
+
+srun --nodes=1 --ntasks=1 -w "$head_node" --export=ALL,WANDB_PROJECT="$WANDB_PROJECT" \
     accelerate launch --main_process_ip="$head_node_ip" \
                           --main_process_port="$port" \
                           --num_processes=4 \
@@ -40,7 +42,7 @@ worker_num=$((SLURM_JOB_NUM_NODES - 1))
 for ((i = 1; i <= worker_num; i++)); do
     node_i=${nodes_array[$i]}
     echo "Starting WORKER $i at $node_i"
-    srun --nodes=1 --ntasks=1 -w "$node_i" \
+    srun --nodes=1 --ntasks=1 -w "$node_i" --export=ALL,WANDB_PROJECT="$WANDB_PROJECT" \
         accelerate launch --main_process_ip="$head_node_ip" \
                           --main_process_port="$port" \
                           --num_processes=4 \
