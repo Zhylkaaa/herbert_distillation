eac2aedbb18407ee38fa7944c37c0cb1263e7ab3
Dima <zhylko.dima@gmail.com>
2024-04-25 15:33:03 -0400

    add full klej evaluation

--- download_klej.sh
+++ download_klej.sh
@@ -9,7 +9,7 @@ DATA_PATH="klej_data"
 mkdir -p "${DATA_PATH}"
 
 # Download KLEJ datasets
-task_names=("nkjp-ner" "cbd" "polemo2.0-in" "polemo2.0-out")
+task_names=("nkjp-ner" "cdsc-e" "cdsc-r" "cbd" "polemo2.0-in" "polemo2.0-out" "dyk" "psc" "ar")
 for task_name in "${task_names[@]}"; do
     echo "Downloading: ${task_name}"
     curl -o "${DATA_PATH}/klej_${task_name}.zip" "https://klejbenchmark.com/static/data/klej_${task_name}.zip"
